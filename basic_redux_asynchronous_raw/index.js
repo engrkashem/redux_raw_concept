d973fc47471c5f3e52bf9674b3265ac0ec701583
engrkashem <kashemaust@gmail.com>
2023-12-18 16:17:34 +0600

    handling asynchronous operation with raw reduc/vanilla redux. basic concept

---
+++ basic_redux_asynchronous_raw/index.js
@@ -0,0 +1,65 @@
+const { createStore, applyMiddleware } = require("redux");
+const {delayActionMiddleware, todoFetchMiddleware}= require('./middlewares');
+
+
+// initial state
+const initialState={
+    todos:[],
+}
+
+
+
+// reducer
+const todoReducer=(state=initialState, action)=>{
+    switch (action.type) {
+        case 'todos/todoAdded':
+            return {
+                ...state,
+                todos:[
+                    ...state.todos,
+                    {
+                        title:action.payload
+                    }
+                ]
+            };
+        
+        case 'todos/todoLoaded':
+            return {
+                ...state,
+                todos:[
+                    ...state.todos,
+                    ...action.payload
+                ]
+            };
+    
+        default:
+            break;
+    }
+}
+
+
+
+// store
+const store=createStore(todoReducer, applyMiddleware(delayActionMiddleware,todoFetchMiddleware));
+
+
+// subscribe to store to state change
+store.subscribe(()=>{
+    console.log(store.getState());
+});
+
+
+
+// dispatch action
+// store.dispatch(
+//     {
+//         type:'todos/todoAdded',
+//         payload:"Let's learn Redux from Scratch in RAW manner",
+//     }
+// )
+store.dispatch(
+    {
+        type:'todos/fetchTodos',
+        
+    }
+)
---
+++ basic_redux_asynchronous_raw/middlewares.js
@@ -0,0 +1,46 @@
+const fetch = require('node-fetch');
+
+
+const delayActionMiddleware=(store)=>(next)=>(action)=>{
+    if(action.type === 'todos/todoAdded'){
+
+        console.log("I'm delaying you. Just my wish. !!!");
+
+        setTimeout(() => {
+            next(action);
+        }, 2000);
+
+        return;
+    }
+
+    return next(action);
+};
+
+const todoFetchMiddleware=(store)=>(next)=>async(action)=>{
+
+    if(action.type ==='todos/fetchTodos'){
+
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=5');
+
+        const todos= await response.json();
+
+        store.dispatch({
+            type:"todos/todoLoaded",
+            payload:todos
+        })
+
+        console.log(`Number of updated todos is ${store.getState().todos.length}`)
+
+        return;
+
+    }
+
+    return next(action)
+
+}
+
+
+module.exports={
+    delayActionMiddleware,
+    todoFetchMiddleware
+}
